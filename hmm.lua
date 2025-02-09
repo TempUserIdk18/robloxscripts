0dc027dbda8c23707214967a35958cbab5735f30
TempUserIdk18 <148344080+TempUserIdk18@users.noreply.github.com>
2025-02-09 16:43:01 +0500

    Update hmm.lua

--- hmm.lua
+++ hmm.lua
@@ -22,11 +22,11 @@ end
 
 if gg ~= nil then
 for i,v in pairs(game.Players:GetPlayers()) do
-AnimationId = "148840371"
-local Anim = Instance.new("Animation")
-Anim.AnimationId = "rbxassetid://"..AnimationId
-local k = game.Players.LocalPlayer.Character.Humanoid:LoadAnimation(Anim)
-k:Play()
-k:AdjustSpeed(8)
+AnimationId2 = "148840371"
+local Anim2 = Instance.new("Animation")
+Anim2.AnimationId = "rbxassetid://"..AnimationId2
+local k2 = game.Players.LocalPlayer.Character.Humanoid:LoadAnimation(Anim2)
+k2:Play()
+k2:AdjustSpeed(8)
 end
 end
