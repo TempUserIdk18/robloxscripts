acc9ceae8327c515c924505b11e50928963b6e13
TempUserIdk18 <148344080+TempUserIdk18@users.noreply.github.com>
2025-02-25 09:14:28 +0500

    Create FEPack.lua

---
+++ FEPack.lua
@@ -0,0 +1,15 @@
+-- FE Pack v2
+-- It has everything you need!
+
+
+
+
+
+-- Gyst Bypasser (don't forget to set vietnam!)
+loadstring(game:HttpGet("https://pastefy.app/IIhFSYBL/raw"))()
+
+-- Animations (R6/R15)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TempUserIdk18/robloxscripts/refs/heads/main/Animations.lua"))()
+
+-- wisl'i (Universal Project)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wisl884/wisl-i-Universal-Project1/main/Wisl'i%20Universal%20Project.lua", true))()
