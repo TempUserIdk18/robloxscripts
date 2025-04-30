1bda69c56c6352d8ea19d3520bc6b30ad27c3ce9
TempUserIdk18 <148344080+TempUserIdk18@users.noreply.github.com>
2025-04-30 11:37:54 +0500

    Create StarlightUnivS.lua

---
+++ StarlightUnivS.lua
@@ -0,0 +1,12 @@
+if game.PlaceId==85018513321398 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/TempUserIdk18/robloxscripts/refs/heads/main/starlightde.lua"))()
+end
+if game.PlaceId==13864661000 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TempUserIdk18/robloxscripts/refs/heads/main/starlightBreak.lua"))()
+end
+if game.PlaceId==105547852640520 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TempUserIdk18/robloxscripts/refs/heads/main/trollpintower.lua"))()
+end
+if game.PlaceId==142823291 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TempUserIdk18/robloxscripts/refs/heads/main/v3rmu_mm2.lua"))()
+end
